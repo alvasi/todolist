481c2f54d1301e063ad334b47d92f1d78455f69b
Alva Si <alva.si19@imperial.ac.uk>
2026-03-21 17:30:58 +0800

    chore: add basic github workflow yml

--- react-with-flask/api/app/api.py
+++ react-with-flask/api/app/api.py
@@ -1,13 +1,14 @@
 import time
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
 
     @app.route('/')
     def index():
         return 'Hello, World!'
-    
+
     @app.route('/api/time')
     def get_current_time():
         return {'time': time.time()}
