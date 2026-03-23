7636b92d2aa1b3d8d049023910be4b096c659251
Alva Si <alva.si19@imperial.ac.uk>
2026-03-24 01:29:53 +0800

    chore: add dockerfiles and docker-compose, change workflow to test the docker build

--- react-with-flask/vite.config.js
+++ react-with-flask/vite.config.js
@@ -1,12 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
+    host: true,
     proxy: {
-      '/api': 'http://127.0.0.1:5001',
-    },
-  },
+      '/api': {
+        target: 'http://api:5001',
+        changeOrigin: true,
+      }
+    }
+  }
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': 'http://0.0.0.0:5001',
+//     },
+//   },
+// })
